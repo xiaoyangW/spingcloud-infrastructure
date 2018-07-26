533379f08df59c3ab5d7089b565e488f67fc4684
xiaoyang <1483863489@qq.com>
2018-07-26 16:34:51 +0800

    修改服务注册中心

--- eureka/src/main/java/com/xiaoyang/structure/Application.java
+++ eureka/src/main/java/com/xiaoyang/structure/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
+ * @author WXY
  * eureka application
  */
 @SpringBootApplication
