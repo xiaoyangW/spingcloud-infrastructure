c853786e7a7402539149c2669273f6b102969f81
xiaoyang <1483863489@qq.com>
2018-08-03 18:46:16 +0800

    整理授权中心--正在知识储备中🙃

--- auth-service/src/main/java/com/xiaoyang/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/xiaoyang/authservice/AuthServiceApplication.java
@@ -2,8 +2,15 @@ package com.xiaoyang.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+/**
+ * @author WXY
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAuthorizationServer
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
