4ae55248e38db1fa06a02628801ca573c9672425
wenxiaoyang <xiaoyang.wem@arvato.com>
2019-03-29 16:27:42 +0800

    auth

---
+++ auth-service/src/main/java/com/xiaoyang/authservice/config/OAuth2AuthorizationConfig.java
@@ -0,0 +1,10 @@
+package com.xiaoyang.authservice.config;
+
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+/**
+ * @author xiaoyang.wen
+ * @date 2019/3/29 16:10
+ */
+public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+}
--- auth-service/src/main/java/com/xiaoyang/authservice/domain/User.java
+++ auth-service/src/main/java/com/xiaoyang/authservice/domain/User.java
@@ -3,6 +3,10 @@ package com.xiaoyang.authservice.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 /**
  * @author WXY
@@ -11,9 +15,55 @@ import lombok.ToString;
 @Setter
 @Getter
 @ToString
-public class User {
+public class User implements UserDetails {
 
     private String name;
     private String psw;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return psw;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    /**
+     * 用户是否过期
+     * @return Boolean
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    /**
+     * 用户是否锁定
+     * @return boolean
+     */
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    /**
+     * 证书未过期
+     * @return boolean
+     */
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
