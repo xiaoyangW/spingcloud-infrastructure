cff8caa8c89753f18c5bb4dcfc56b85343a18524
xiaoyang <1483863489@qq.com>
2019-04-20 22:00:41 +0800

    删除多余引入的包

--- business-service/src/main/java/com/xiaoyang/business/feign/IAuthService.java
+++ business-service/src/main/java/com/xiaoyang/business/feign/IAuthService.java
@@ -2,10 +2,13 @@ package com.xiaoyang.business.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import reactor.core.publisher.Mono;
 
 import java.security.Principal;
+import java.util.Map;
 
 @Repository
 @FeignClient(name = "auth-service")
@@ -14,4 +17,6 @@ public interface IAuthService {
     @RequestMapping(method = RequestMethod.GET,value = "/user/principal")
     Principal getUser();
 
+    @RequestMapping(method = RequestMethod.POST,value = "/user/add")
+    Boolean add(@RequestBody Map<String,String> user);
 }
