8f85defab1ec00101ceaa7e47e01c2eae0be565a
xiaoyang <1483863489@qq.com>
2019-04-21 20:41:27 +0800

    删除多余引入的包

--- business-service/src/main/java/com/xiaoyang/business/controller/TestController.java
+++ business-service/src/main/java/com/xiaoyang/business/controller/TestController.java
@@ -2,12 +2,14 @@ package com.xiaoyang.business.controller;
 
 import com.xiaoyang.business.feign.IAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
@@ -26,7 +28,10 @@ public class TestController {
     }
 
     @GetMapping("/test")
-    public Mono<String> test(){
-        return Mono.just("sssss");
+    public Mono<Boolean> test(){
+        Map<String, String> user = new HashMap<>(2);
+        user.put("name","user1");
+        user.put("psw",new BCryptPasswordEncoder().encode("123456"));
+        return Mono.just(authService.add(user));
     }
 }
