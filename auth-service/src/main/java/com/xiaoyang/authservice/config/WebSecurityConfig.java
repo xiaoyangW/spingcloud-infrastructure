08f64dc4a558457514b5643ce21a6a928b37b9b6
wenxiaoyang <xiaoyang.wem@arvato.com>
2019-04-26 10:59:37 +0800

    修改对象类名

--- auth-service/src/main/java/com/xiaoyang/authservice/config/WebSecurityConfig.java
+++ auth-service/src/main/java/com/xiaoyang/authservice/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
@@ -19,7 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsServiceImpl userDetailsService;
+    private final UserDetailsService userDetailsService;
 
     @Autowired
     public WebSecurityConfig(UserDetailsServiceImpl userDetailsService) {
