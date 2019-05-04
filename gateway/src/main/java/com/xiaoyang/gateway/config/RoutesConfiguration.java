3f19d66f61f806be9cfc031af0619c8390f8dbb1
xiaoyang <1483863489@qq.com>
2019-05-04 15:39:35 +0800

    添加编码形式配置spring-cloud-gateway

---
+++ gateway/src/main/java/com/xiaoyang/gateway/config/RoutesConfiguration.java
@@ -0,0 +1,25 @@
+/*
+package com.xiaoyang.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+*/
+/**
+ * @author WXY
+ *//*
+
+@Configuration
+public class RoutesConfiguration {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes().route(predicateSpec ->
+                predicateSpec.path("/auth/**").uri("lb://auth-service").id("auth-service")
+        ).build();
+    }
+
+}
+*/
